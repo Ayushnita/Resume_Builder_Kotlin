b5f9f56e2e94ba8299c0fcf9b0aa63f66aaf6404
Ayush Agrawal <ayushnita.ece@gmail.com>
2021-01-21 23:19:24 +0530

    preoject setup

---
+++ src/main/kotlin/com/openuniquesolution/ResumeBuilder/ResumeBuilderApplication.kt
@@ -0,0 +1,11 @@
+package com.openuniquesolution.ResumeBuilder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResumeBuilderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResumeBuilderApplication>(*args)
+}
